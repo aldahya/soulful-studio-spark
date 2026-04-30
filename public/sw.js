6b3d7096643a097e2d6cbdfb7c1a4f694c77dc2a
aldahya <talesus029@gmail.com>
2026-04-30 13:39:33 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,13 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = 'aldahia-v2'; // قمنا بتغيير الإصدار هنا
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // يجبر المتصفح على تفعيل التحديث فوراً
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim()); // يسيطر على الصفحة فوراً
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
